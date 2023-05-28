83bd538eeaa50f79dadc8224ae7657a72a24bec6
sgvugaurav <87558201+sgvugaurav@users.noreply.github.com>
2023-05-28 21:26:48 +0530

    setup express and ejs

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 # Mantra-Computer
-
-[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/stackblitz-starters-8tsc44)
--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require('express');
+const path = require('path');
 
 const PORT = process.env.PORT || 3000;
 const hostname = '127.0.0.1';
 
 const app = express();
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.render('index');
 });
 
 app.listen(PORT, hostname, () => {
